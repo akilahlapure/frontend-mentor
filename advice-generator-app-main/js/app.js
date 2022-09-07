daed0835725c0d3fafb12a4497facd31eb739d30
akickandopen <akilah.lapure@gmail.com>
2022-09-07 18:08:54 +0400

    add function to render new advice

--- advice-generator-app-main/js/app.js
+++ advice-generator-app-main/js/app.js
@@ -1,3 +1,22 @@
-function getAdvice(){
+async function getAdvice(){
+    let url = 'https://api.adviceslip.com/advice';
+
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function renderAdvice(){
+    let data = await getAdvice();
+    let advice_id = data.slip.id;
+    let advice_quote = data.slip.advice;
     
+    let id = document.getElementById("adviceID");
+    let quote = document.getElementById("adviceQuote")
+
+    id.innerHTML = advice_id;
+    quote.innerHTML = advice_quote;
 }
